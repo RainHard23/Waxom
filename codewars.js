4da75a710a6a5213d8c469df088cb014f34adfbe
RainHard23 <moiredmi7@gmail.com>
2022-07-07 21:27:13 +0300

    7

--- codewars.js
+++ codewars.js
@@ -34,3 +34,25 @@ function mergeArrays(arr1, arr2) {
         });
         return Array;
   }
+//   5
+const rps = (p1, p2) => {
+    if (p1 === 'rock' && p2 === 'scissors') {
+      return('Player 1 won!');
+    }
+    
+    else if (p1 === 'scissors' && p2 === 'paper') {
+      return('Player 1 won!');
+    }
+    
+    else if (p1 === 'paper' && p2 === 'rock') {
+      return('Player 1 won!');
+    }
+    
+    else if (p1 === p2) {
+      return('Draw!');
+    }
+    
+    else {
+      return('Player 2 won!');
+    }
+  };
